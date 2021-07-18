6948736830a00651a68c94cc4c342674b84cd5e1
Bass2Gamer <bastiankrueger.1@web.de>
2021-07-18 16:55:23 +0200

    halb funtkionierende coordinaten

--- Assets/Scripts/ForestGenerator.cs
+++ Assets/Scripts/ForestGenerator.cs
@@ -9,12 +9,31 @@ public class ForestGenerator : MonoBehaviour
 
     public Element[] elements;
 
+
+    const int mapChunkSize = 241;
+    public float noiseScale = 50;
+    public int seed = 5;
+    public int octaves = 12;
+    public float persistance = 0.5f;
+    public float lacunarity = 2;
+    public Vector2 offset = new Vector2(50, 0);
+    public float meshHeightMultiplier = 100;
+    public AnimationCurve meshHeightCurve ;
+
+
+    float[,] noiseMap;
+
+    void Awake()
+    {
+        noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+    }
+
     private void Start()
     {
 
-        for (int x = -10; x < forestSize; x += elementSpacing)
+        for (int x = 0; x < forestSize; x += elementSpacing)
         {
-            for (int z = -50; z < forestSize; z += elementSpacing)
+            for (int z = 0; z < forestSize; z += elementSpacing)
             {
 
                 for(int i = 0; i < elements.Length; i++)
@@ -24,8 +43,9 @@ public class ForestGenerator : MonoBehaviour
 
                     if (element.CanPlace())
                     {
-                        Vector3 position = new Vector3(x, 61.7f, z); //0f durch terrain height ersetzen
-                        Vector3 offset = new Vector3(Random.Range(-0.75f, 0.75f), 0f, Random.Range(-0.75f, 0.75f));
+                        Debug.Log(-noiseMap[x,z]*25);
+                        Vector3 position = new Vector3(x, meshHeightCurve.Evaluate(noiseMap[x, z]) * meshHeightMultiplier, z); //0f durch terrain height ersetzen
+                        Vector3 offset = new Vector3(Random.Range(-0.75f, 0.75f), 65f, Random.Range(-0.75f, 0.75f));
                         Vector3 rotation = new Vector3(Random.Range(0, 5f), Random.Range(0, 360f), Random.Range(0, 5f));
                         Vector3 scale = Vector3.one * Random.Range(0.75f, 1.2f);
 
